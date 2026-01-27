c0621d71b61b27b98dacd523fec712fe4ad3b685
JemisKevadiya <jemiskevadiya9@gmail.com>
2026-01-28 00:53:53 +0530

    Hyperparameter trianing

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,7 +41,41 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostClassifier()
             }
 
-            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "Random Forest": {
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression": {
+
+                },
+                "K-Neighbors Classifier": {
+                    'n_neighbors':[5,7,9,11],
+                    'weights':['uniform','distance']
+                },
+                "XGB Classifier": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoost Classifier": {
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Classifier": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+
+            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
 
             # To get the best model score from the dictionary
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 import src.exception as exception
 
@@ -19,7 +20,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise exception.CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test, models):
+def evaluate_models(X_train, y_train,X_test,y_test, models, params):
     try:
         report = {}
 
@@ -27,7 +28,14 @@ def evaluate_models(X_train, y_train,X_test,y_test, models):
             model = list(models.values())[i]
             model_name = list(models.keys())[i]
 
+            para = params[list(models.keys())[i]]
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+
+            # model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
